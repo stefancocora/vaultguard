de37f05f6a0151306fc30f391e4818f46dbf9198
Stefan Cocora <stefan.cocora@gmail.com>
2017-08-30 07:03:09 +0100

    Adding the DescribeContainerInstances call

--- pkg/discover/aws/ecs.go
+++ pkg/discover/aws/ecs.go
@@ -187,19 +187,109 @@ func (ec AwsEcsConf) describeTasks(td []string) ([]string, []ecs.Failure, error)
 		}
 	}
 
-	// return instance ARNs
+	// step: return instance ARNs
 	for res := range result.Tasks {
 		ia = append(ia, *result.Tasks[res].ContainerInstanceArn)
 	}
 	for f := range result.Failures {
 		iaFailures = append(iaFailures, *result.Failures[f])
 	}
-	// no fialure
+	// partial failures test
 	if len(iaFailures) == 0 {
 		return ia, []ecs.Failure{}, nil
-		// partial failure
 	}
 	return ia, iaFailures, nil
+}
+
+// describeContInst interogates the DescribeContainerInstances AWS ECS API endpoint to retrieve the container instance ids
+//
+// IN
+//
+// OUT
+func (ec AwsEcsConf) describeContInst(ia []string) ([]string, []ecs.Failure, error) {
+
+	var iid []string
+	var iidFailures []ecs.Failure
+
+	// step: create a session
+	sess := session.Must(session.NewSession())
+
+	found := false
+	for i := range supportedRegions {
+		if supportedRegions[i] == ec.Region {
+			found = true
+		}
+	}
+	if !found {
+		errm := fmt.Sprintf("ecs: unsupported region %v", ec.Region)
+		return []string{}, []ecs.Failure{}, errors.New(errm)
+	}
+
+	// step: create a svc session
+	svc := ecs.New(sess, aws.NewConfig().WithRegion(ec.Region))
+
+	// step: prepare inputs
+	var arns []*string
+
+	for i := range ia {
+		arns = append(arns, &ia[i])
+	}
+
+	input := &ecs.DescribeContainerInstancesInput{
+		Cluster:            &ec.Cluster,
+		ContainerInstances: arns,
+	}
+
+	result, err := svc.DescribeContainerInstances(input)
+	// complete failure cases
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecs.ErrCodeServerException:
+				// fmt.Println(ecs.ErrCodeServerException, aerr.Error())
+				return []string{}, []ecs.Failure{}, aerr
+			case ecs.ErrCodeClientException:
+				// fmt.Println(ecs.ErrCodeClientException, aerr.Error())
+				return []string{}, []ecs.Failure{}, aerr
+			case ecs.ErrCodeInvalidParameterException:
+				// fmt.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+				return []string{}, []ecs.Failure{}, aerr
+			case ecs.ErrCodeClusterNotFoundException:
+				// fmt.Println(ecs.ErrCodeClusterNotFoundException, aerr.Error())
+				return []string{}, []ecs.Failure{}, aerr
+			default:
+				// fmt.Println(aerr.Error())
+				return []string{}, []ecs.Failure{}, aerr
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			// fmt.Println(err.Error())
+			return []string{}, []ecs.Failure{}, aerr
+		}
+	}
+
+	if dbgEcsPkg {
+		// extra debug
+		// log.Printf("results: %v", result.ContainerInstances)
+		for res := range result.ContainerInstances {
+			log.Printf("ecs: discovered container id: %#v", *result.ContainerInstances[res].Ec2InstanceId)
+		}
+	}
+
+	// step: return instance ids
+	for res := range result.ContainerInstances {
+		iid = append(iid, *result.ContainerInstances[res].Ec2InstanceId)
+	}
+	// partial failures test
+	if len(iidFailures) == 0 {
+		return iid, []ecs.Failure{}, nil
+	}
+
+	// log.Printf("all instance ids: %v", result)
+	// log.Println(iid)
+	// log.Println(iidFailures)
+	return iid, iidFailures, nil
 }
 
 // Discover is the ecs pkg entrypoint
@@ -229,9 +319,18 @@ func Discover(ec []AwsEcsConf) (AwsEcsDsc, error) {
 		if len(iaf) != 0 {
 			log.Printf("ecs: partial failures when running DescribeTasks(): %v", iaf)
 		}
-		log.Printf("all instance ARNs: %v", ia)
 
-		// step: describe container instances and get priv ip ?
+		// step: describe container instances and get instance id
+		iid, iipsf, err := ec[i].describeContInst(ia)
+		if err != nil {
+			errm := fmt.Sprintf("ecs: error received when describing container instances: %v", err)
+			return AwsEcsDsc{}, errors.New(errm)
+		}
+		if len(iipsf) != 0 {
+			log.Printf("ecs: partial failures when running DescribeContainerInstances(): %v", iipsf)
+		}
+
+		log.Printf("all instance ids: %v", iid)
 
 		// this should return the struct of discovered vault servers
 		return AwsEcsDsc{}, nil
